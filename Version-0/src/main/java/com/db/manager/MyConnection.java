91490f58346b5932b526e42f0c9f24263cc6acc9
Suvendu <suvendu.chok@gmail.com>
2023-06-15 15:47:26 +0530

    Besic home page ready | best design patern ready| all are working successfull

---
+++ Version-0/src/main/java/com/db/manager/MyConnection.java
@@ -0,0 +1,30 @@
+package com.db.manager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MyConnection {
+
+	private Connection con;
+	private PreparedStatement ps;
+
+	public MyConnection(String url, String username, String password) throws Exception {
+		System.out.println("Object Created...");
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		this.con = DriverManager.getConnection(url, username, password);
+	}
+
+	public int runMysqlQuery(String sql) throws Exception {
+		ps = this.con.prepareStatement(sql);
+		return ps.executeUpdate();
+	}
+
+	public ResultSet fetchData(String sql) throws Exception {
+		ps = this.con.prepareStatement(sql);
+		return ps.executeQuery();
+
+	}
+
+}
---
+++ Version-0/src/main/java/com/myservlet/FetchAll.java
@@ -0,0 +1,54 @@
+package com.myservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.db.manager.MyConnection;
+
+@WebServlet("/FetchAll")
+public class FetchAll extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service (HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+
+		try {
+			MyConnection dao = new MyConnection("jdbc:mysql://localhost:3306/movie", "root", "");
+			String sql = "SELECT * FROM movie_details_list ORDER BY datetime DESC";
+			ResultSet rs = dao.fetchData(sql);
+			
+			while(rs.next()) {
+				String mname = rs.getString("movie_name");
+				String mpic = rs.getString("image");
+				String quality = rs.getString("Quality");
+				
+				out.println("			<div class=\"col-sm-4\">\r\n"
+						+ "				<div class=\"card m-2\">\r\n"
+						+ "					<img width=\"300\" height=\"444\" src=\""+mpic+"\" alt=\"\" class=\"card-img-top\"\r\n"
+						+ "						height=\"200px\">\r\n"
+						+ "					<div class=\"card-footer\">\r\n"
+						+ "						<small class=\"text-muted\">"+mname+" "+quality+"</small>\r\n"
+						+ "					</div>\r\n"
+						+ "				</div>\r\n"
+						+ "			</div>");
+				
+			}
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
